18323b6260d869a709d38ccc05389f28dff6927a
Kevin O'Brien <kdobrien22@gmail.com>
2022-03-11 15:59:49 -0800

    added starter code from demos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { useAuth } from "./custom-hooks";
+import { Form } from "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hi I'm App</div>
-      </header>
+  const { token, isLoggedIn, logout } = useAuth();
+
+  const route = (
+    <div>
+      <div>token value is: {token || "''"}</div>
+      <div>isLoggedIn? {isLoggedIn.toString()}</div>
+      <Form />
+      <button onClick={() => logout()}>Logout</button>
     </div>
+  );
+
+  return (
+    <Router>
+      <nav>
+        {isLoggedIn && <Link to="/">logged in link</Link>}
+        {!isLoggedIn && <Link to="/">logged out link</Link>}
+      </nav>
+
+      <Route path="/" component={() => route} />
+
+      {/* this would be an example of an UNAUTHENTICATED ROUTE */}
+      {!isLoggedIn && (
+        <Route
+          path="/pizza"
+          component={() => <div>not logged in with pizza</div>}
+        />
+      )}
+    </Router>
   );
 }
 
