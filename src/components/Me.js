f43d393cb94a6dc082f6113788424a28ea248aa6
Kevin O'Brien <kdobrien22@gmail.com>
2022-03-15 13:49:29 -0700

    delete posts is handled in Me component

--- src/components/Me.js
+++ src/components/Me.js
@@ -32,33 +32,86 @@ export default function Me() {
 
     fetchMe();
   }, [token]);
-  return (
-    <div>
-      <h1>{username}</h1>
-      {messages &&
-        messages.map((message) => {
-          const { _id, content, post, fromUser } = message;
 
-          return (
-            <div key={_id}>
-              <p>{content}</p>
-              <p>{post.title}</p>
-              <p>{fromUser.username}</p>
-            </div>
-          );
-        })}
-      {userPosts &&
-        userPosts.map((post) => {
-          const { _id, title, description, price } = post;
-          return (
-            <div key={_id}>
-              <h4>{title}</h4>
-              <p>{description}</p>
-              <label>Price</label>
-              <span>{price}</span>
-            </div>
-          );
-        })}
-    </div>
+  const activePosts = userPosts.filter((post) => post.active);
+  const inactivePosts = userPosts.filter((post) => !post.active);
+
+  async function deletePost(postId) {
+    try {
+      const response = await fetch(
+        `https://strangers-things.herokuapp.com/api/2202-FTB-PT-WEB-FT/posts/${postId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const { success } = await response.json();
+
+      if (success) {
+        const filteredPosts = userPosts.map((post) => {
+          if (post._id === postId) {
+            post.active = false;
+          }
+          return post;
+        });
+        setUserPosts(filteredPosts);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <section>
+      <div>
+        <h1>{username}</h1>
+        <h1>Current Messages</h1>
+        {messages &&
+          messages.map((message) => {
+            const { _id, content, post, fromUser } = message;
+
+            return (
+              <div key={_id}>
+                <p>{content}</p>
+                <p>{post.title}</p>
+                <p>{fromUser.username}</p>
+              </div>
+            );
+          })}
+        <h1>My Posts</h1>
+        <h3>Active Posts</h3>
+        {activePosts &&
+          activePosts.map((post) => {
+            const { _id, title, description, price } = post;
+            return (
+              <div key={_id}>
+                <h4>{title}</h4>
+                <p>{description}</p>
+                <label>Price</label>
+                <span>{price}</span>
+                <button onClick={() => deletePost(post._id)}>
+                  Delete Post
+                </button>
+              </div>
+            );
+          })}
+
+        <h3>Inactive Posts</h3>
+        {inactivePosts &&
+          inactivePosts.map((post) => {
+            const { _id, title, description, price } = post;
+            return (
+              <div key={_id}>
+                <h4>{title}</h4>
+                <p>{description}</p>
+                <label>Price</label>
+                <span>{price}</span>
+              </div>
+            );
+          })}
+      </div>
+    </section>
   );
 }
